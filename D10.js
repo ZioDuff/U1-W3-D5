62900f8303321319dacca9464c7b197653e1e499
Jacopo <jajodemaio@gmail.com>
2024-03-15 16:43:17 +0100

    add exercesi 22

--- D10.js
+++ D10.js
@@ -374,9 +374,14 @@ selectAllTagId()
   Scrivi una funzione che, tramite un ciclo, stampa in console il testo contenuto in ogni tag <td> all'interno della pagina.
 */
 const textTd = function () {
-  const allTd = document.getElementsByTagName("td")
+  const allTd = document.querySelectorAll("td")
+  allTd.forEach((td) => {
+    console.log(td.textContent)
+  })
 }
 console.log("es.22")
+textTd()
+
 /* ESERCIZIO 23
   Scrivi una funzione per aggiungere un background di colore rosso a ogni link all'interno della pagina.
 */
